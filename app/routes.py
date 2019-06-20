fe4e879bd8f4bfadeacb12099a536fca0149d8bd
Sarat <saratsarat7@gmail.com>
2019-06-20 23:47:34 +0530

    Working Api Web Server on /SpeechApi

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
-from app import app
 from flask import render_template
+from app import app
+
+
+import sys
+import os
+sys.path.append(os.path.abspath("/media/sarat/Sarat/Development/Python/Flask WebPage/app"))
+from GoogleApi import *
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
-@app.route('/page')
+    # user = {'username': 'Sarat'}
+    # return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
+
+@app.route('/SpeechApi')
 def page():
-    return "Some page !!"
+    data = process_voice()
+    return "You said : "+data
+
+
+@app.route("/multiply/<int:key>/", methods=['GET'])
+def multiply(key):
+    return str(key*key)
