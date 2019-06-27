bdf43bef23a97a60a5cc7867e0d4621af519fac1
Sarat <saratsarat7@gmail.com>
2019-06-27 22:13:22 +0530

    Deleted Everything

--- gTTStest.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-import os
-tts = gTTS(text='Please say patient name', lang='en-us')
-tts.save("good.mp3")
-os.system("mpg321 good.mp3")
--- microblog.py
+++
@@ -1,3 +0,0 @@
-from app import app
-
-# app=Flask(__name__, static_folder='static',  template_folder='templates')
